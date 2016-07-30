717a2479e0b1733e3fa23666c0c7b0da1e11e06d
tuddman <scott.tudd@gmail.com>
2016-07-30 09:25:40 -0400

    updates dependencies version(s)

--- project.clj
+++ project.clj
@@ -1,18 +1,18 @@
 (defproject clj-wamp "2.0.0-SNAPSHOT"
   :description "The WebSocket Application Messaging Protocol for Clojure"
-  :url "https://github.com/cgmartin/clj-wamp"
+  :url "https://github.com/tuddman/clj-wamp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.clojure/core.incubator "0.1.2"]
-                 [org.clojure/tools.logging "0.2.6"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.2.385"]
+                 [org.clojure/core.incubator "0.1.4"]
+                 [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/data.codec "0.1.0"]
-                 [http-kit "2.1.21-alpha2"]  ; "2.1.5"]
-                 [cheshire "5.2.0"]
-                 [stylefruits/gniazdo "0.4.0"]]
-  :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+                 [http-kit "2.2.0"]  
+                 [cheshire "5.6.3"]
+                 [stylefruits/gniazdo "1.0.0"]]
+  :profiles {:1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :dev {:dependencies [[log4j "1.2.17" :exclusions [javax.mail/mail
                                                                javax.jms/jms
                                                                com.sun.jdmk/jmxtools
